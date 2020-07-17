46a6f08dae512a277f55f1cd6e0d5fa6c9dfcddc
Michael <michael_schneemann@hotmail.de>
2020-07-17 22:03:47 +0200

    general layout, welcoming page, login form created

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session, render_template
+from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -22,4 +22,22 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    mainHeading = "Welcome to Bookuru!"
+    return render_template("index.html", mainHeading = mainHeading)
+
+@app.route("/registration")
+def registration():
+    mainHeading = "Please register here"
+    return render_template("registration.html", mainHeading = mainHeading)
+
+@app.route("/bookSearch")
+def bookSearch():
+    mainHeading = "Look up your next book"
+    return render_template("bookSearch.html", mainHeading = mainHeading)
+
+@app.route("/login", methods =["POST"])
+def login():
+    mainHeading = "Thanks for logging in"
+    username = request.form.get("username")
+    password = request.form.get("password")
+    return render_template("login.html", mainHeading = mainHeading, username = username, password = password)
