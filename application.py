ded2f767f05fd45171ff5232e44d2ca9e1c35091
Michael <michael_schneemann@hotmail.de>
2020-07-18 00:49:41 +0200

    login, registration layout,routes and forms created

--- application.py
+++ application.py
@@ -16,28 +16,51 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Set up database
+# Set up database on hekoru
 engine = create_engine(os.getenv("DATABASE_URL"))
+# make sure user sessions are seperated from another if happening at the same time
 db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
     mainHeading = "Welcome to Bookuru!"
-    return render_template("index.html", mainHeading = mainHeading)
+    username = "defaultUser"
 
-@app.route("/registration")
+    if username == "defaultUser":
+        loggedInUserTxt = "Not logged in yet"
+    else:
+        loggedInUserTxt = f"Logged in as: {username}"
+    return render_template("index.html", mainHeading = mainHeading, loggedInUserTxt = loggedInUserTxt)
+
+@app.route("/registration", methods=["GET", "POST"])
 def registration():
-    mainHeading = "Please register here"
-    return render_template("registration.html", mainHeading = mainHeading)
+    if request.method == "POST":
+        mainHeading = "Your Registration was successful"
+        return render_template("index.html", mainHeading = mainHeading)
+    else:
+        mainHeading = "Please register here"
+        return render_template("registration.html", mainHeading = mainHeading)
+
+@app.route("/login", methods =["POST"])
+def login():
+    username = "defaultUser"
+
+    username = request.form.get("username")
+    password = request.form.get("password")
+    mainHeading = f"Welcome,  {username} . Thanks for logging in!"
+
+    # ----- check whether username and password are part of the db -----
+    # ...
+    # -------------
+
+    if username == "defaultUser":
+        loggedInUserTxt = "Not logged in yet"
+    else:
+        loggedInUserTxt = f"Logged in as: {username}"
+
+    return render_template("login.html", mainHeading = mainHeading, username = username, password = password, loggedInUserTxt = loggedInUserTxt)
 
 @app.route("/bookSearch")
 def bookSearch():
     mainHeading = "Look up your next book"
     return render_template("bookSearch.html", mainHeading = mainHeading)
-
-@app.route("/login", methods =["POST"])
-def login():
-    mainHeading = "Thanks for logging in"
-    username = request.form.get("username")
-    password = request.form.get("password")
-    return render_template("login.html", mainHeading = mainHeading, username = username, password = password)
